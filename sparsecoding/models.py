8aed08b8ad2df4386ea3ba80b9a8993c4c806e25
belsten <afbelsten@gmail.com>
2022-05-04 09:24:25 -0700

    SparseCoding model refactor and removal of obsolete methods 1) formatted inline documentation to be consistent with inference method documentation. 2) removed inference parameters from SparseCoding class. 3) renamed variables to be consistent throughout the package (ie self.D to self.dictionary) 4) added method to compute energy, also fixed bug (energy should be divided by batch_size (correct units))
    examples/sparse_coding notebook
    1) make use of SparseCoding refactor introduced in this commit
    2) visualize the dictionary using sparsecoding.visualize
    3) short-run of sparse coding on 16x16 image patches to demonstrate gabor-like basis learned and energy decreases (could probably be hyper-parameter optimized more but that is for future)

--- sparsecoding/models.py
+++ sparsecoding/models.py
@@ -1,110 +1,97 @@
 import numpy as np
 import torch 
-
 from torch.utils.data import DataLoader
 import pickle as pkl
 
 
 class SparseCoding(torch.nn.Module):
     
-    def __init__(self, inference_method, n_basis, n, lmbda=0.2, a_lr=1e-2, device=None,**kwargs):
-        """
-        Parameters:
+    def __init__(self, inference_method, n_basis, n_features, sparsity_penalty=0.2, device=None,**kwargs):
+        '''
+        Class for learning a sparse code via dictionary learning
+        
+        Parameters
+        ----------
         inference_method - sparsecoding.InferenceMethod
             method for inferring coefficients for each image given the dictionary
-        n_basis - scalar (1,)
-            size of dictionary
-        n - scalar (1,)
-            number of features
-        lmbda - scalar (1,) default=0.2
-            update rate - NOTE: unused with LCA algo. See thresh
-        a_lr - scalar (1,) default=1e-2
-            step size of coefficient inference dynamics
-        thresh - scalar (1,) default=1e-2
-            threshold for shrink prior
-        stop_early - boolean defualt=False
-            evaluate stopping criteria using eps
-        eps - scalar (1,) default=1e-2
-            stopping criteria of norm of difference 
-            between coefficient update - eps or n_iter, whichever 
-            comes first
-        n_itr - scalar (1,) default=100
-            number of iterations to update coefficient estimate
-        device - torch.device defualt=torch.device("cpu")
-            which device to utilize
-        D_lr - scalar (1,) default=1e-2
+        n_basis : scalar (1,)
+            number of basis functions in dictionary
+        n_features : scalar (1,)
+            number of features in data
+        sparsity_penalty : scalar (1,) default=0.2
+            sparsity penalty
+        dictionary_lr : scalar (1,) default=1e-2
             learning rate of dictionary update
-        D_decay - scalar (1,) default=0.
-            scalar coefficient of L2 weight decay on dictionary.
-            If equal to None, no weight decay applied
-        """
+        device : torch.device defualt=torch.device("cpu")
+            which device to utilize
+        '''
         
         super(SparseCoding, self).__init__()
         self.inference_method = inference_method
         self.n_basis = n_basis
-        self.n = n
+        self.n_features = n_features
         self.device = torch.device("cpu") if device is None else device
-        self.n_itr = kwargs.pop('n_itr',100)
-        self.eps = kwargs.pop('eps', 1e-3)
-        self.stop_early = kwargs.pop('stop_early', False)   
-        self.D_decay = kwargs.pop('dict_decay', 0.)   
-        self.thresh = torch.tensor(np.float32(kwargs.pop('thresh',1e-2))).to(self.device) 
-        self.D_lr = torch.tensor(np.float32(kwargs.pop('D_lr',1e-2))).to(self.device)            
-        self.a_lr = torch.tensor(np.float32(a_lr)).to(self.device)
-        self.lmbda = torch.tensor(np.float32(lmbda)).to(self.device)
-        self.D = torch.randn((self.n, self.n_basis)).to(self.device)
-        self.normalize_dict()
+        self.dictionary_lr = torch.tensor(np.float32(kwargs.pop('dictionary_lr',1e-2))).to(self.device)            
+        self.sparsity_penalty = torch.tensor(np.float32(sparsity_penalty)).to(self.device)
+        self.dictionary = torch.randn((self.n_features, self.n_basis)).to(self.device)
+        self.normalize_dictionary()
    
 
-    def update_dict(self,I,a):
-        """
-        Compute gradient of energy function w.r.t. dict elements, and update 
-        ---
-        Parameters:
-        I - torch.tensor (batch_size,n)
-            input images, n_images usually batch size
+    def update_dictionary(self,data,a):
+        '''
+        Compute gradient of energy function w.r.t. dictionary elements, and update 
+
+        Parameters
+        ----------
+        data - torch.tensor (batch_size,n_features)
+            input data
         a - torch.tensor (batch_size,n_basis)
             already-inferred coefficients
-        ---
-        Returns:
-        None
-        """
-        residual = I - torch.mm(self.D,a.t()).t()
-        dD = torch.mm(residual.t(),a) + self.D_decay*self.D
-        self.D = torch.add(self.D, self.D_lr*dD)
+
+        Returns
+        -------
+
+        '''
+        residual = data - torch.mm(self.dictionary,a.t()).t()
+        ddictionary = torch.mm(residual.t(),a)
+        self.dictionary = torch.add(self.dictionary, self.dictionary_lr*ddictionary)
         self.checknan()
         
         
-    def normalize_dict(self):
-        """
-        Normalize columns of dictionary matrix D s.t. 
-        ---
-        Parameters:
-        None
-        ---
-        Returns:
-        None
-        """
-        self.D = self.D.div_(self.D.norm(p=2,dim=0))
+    def normalize_dictionary(self):
+        '''
+        Normalize columns of dictionary matrix to unit norm
+        
+        Parameters
+        ----------
+        
+        Returns
+        -------
+        
+        '''
+        self.dictionary = self.dictionary.div_(self.dictionary.norm(p=2,dim=0))
         self.checknan()
         
         
     def learn_dictionary(self,dataset,n_epoch,batch_size):
-        """
+        '''
         Learn dictionary for n_epoch epochs
-        ---
-        Parameters:
-        dataset - torch.utils.data.Dataset 
-            input dataset class
-        n_epoch - scalar (1,)
+
+        Parameters
+        ----------
+        dataset : torch.utils.data.Dataset or scalar (n_samples,n_features)
+            input dataset
+        n_epoch : scalar (1,)
             number of iterations to learn dictionary
-        batch_size - scalar (1,)
+        batch_size : scalar (1,)
             batch size to learn on
         ---
         Returns:
-        loss - scalar (nepoch)
-        """
-        loss = []
+        energies - scalar (nepoch,)
+            
+        '''
+        energies = []
+        
         dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
         iterloader = iter(dataloader)
         for i in range(n_epoch):
@@ -116,84 +103,111 @@ class SparseCoding(torch.nn.Module):
                 batch = next(iterloader)
                 
             # infer coefficients
-            a = self.inference_method(batch, self.D)
+            a = self.inference_method(batch, self.dictionary)
             
             # update dictionary
-            self.update_dict(batch,a)
+            self.update_dictionary(batch,a)
             
             # normalize dictionary
-            self.normalize_dict()
+            self.normalize_dictionary()
             
             # compute current energy
-            l = torch.sum(torch.square(torch.linalg.vector_norm(batch-torch.mm(self.D,a.t()).t(),dim=1)) 
-                          + self.lmbda*torch.abs(a).sum(dim=1))
-            loss.append(l.to(self.device).cpu().detach().numpy())
-        return np.asarray(loss)
+            energy = self.compute_energy(batch,a)
+            
+            energies.append(energy)
+        return np.asarray(energies)
         
         
-    def get_numpy_dict(self):
-        """
+    def compute_energy(self,data,a):
+        '''
+        
+        Parameters
+        ----------
+        data : scalar (batch_size,n_features)
+        
+        a : scalar (batch_size,n_basis)
+            inferred coefficients
+
+        Returns
+        -------
+        float : (1,) 
+            energy 
+        '''
+        batch_size,_ = data.shape
+        
+        MSE_energy = torch.square(torch.linalg.vector_norm(data-torch.mm(self.dictionary,a.t()).t(),dim=1)) 
+        sparsity_energy = self.sparsity_penalty*torch.abs(a).sum(dim=1)
+        total_energy = torch.sum(MSE_energy + sparsity_energy)
+        return total_energy.item()/batch_size
+        
+        
+    def get_numpy_dictionary(self):
+        '''
         return dictionary as numpy array
-        ---
-        Parameters: 
-        None
-        ---
-        Returns:
-        D - scalar (n,n_basis)
-            numpy dictionary
-        """
-        return self.D.cpu().detach().numpy()
+        
+        Parameters
+        ----------
+        
+        Returns
+        -------
+        scalar (n_features,n_basis)
+        '''
+        return self.dictionary.cpu().detach().numpy()
     
     
     def checknan(self,data=torch.tensor(0),name='data'):
-        """
+        '''
         Check for nan values in dictinary, or data
-        ---
-        Parameters:
-        data - torch.tensor default=1
+        
+        Parameters
+        ----------
+        data : torch.tensor default=1
             data to check for nans
-        name - string
+        name : string
             name to add to error, if one is thrown
-        ---
-        Returns:
-        None
-        """
+        
+        Returns
+        -------
+
+        '''
         if torch.isnan(data).any():
             raise ValueError('sparsecoding error: nan in %s.'%(name))
-        if torch.isnan(self.D).any():
+        if torch.isnan(self.dictionary).any():
             raise ValueError('sparsecoding error: nan in dictionary.')
             
             
-    def load_dict(self,filename):
+    def load_dictionary(self,filename):
         '''
         Load dictionary from pkl dump
-        ---
-        Parameters:
-        filename - string
-            file to load as self.D
-        ---
-        Returns:
-        None
+        
+        Parameters
+        ----------
+        filename : string (1,)
+            file to load dictionary from
+        
+        Returns
+        -------
+
         '''
         file = open(filename,'rb')
         nD = pkl.load(file)
         file.close()
-        self.D = torch.tensor(nD.astype(np.float32)).to(self.device) 
+        self.dictionary = torch.tensor(nD.astype(np.float32)).to(self.device) 
         
             
-    def save_dict(self,filename):
+    def save_dictionary(self,filename):
         '''
         Save dictionary to pkl dump
-        ---
-        Parameters:
-        filename - string
-            file to save self.D to
-        ---
-        Returns:
-        None
+
+        Parameters
+        ----------
+        filename : string (1,)
+            file to save current dictionary to
+
+        Returns
+        -------
+
         '''
         filehandler = open(filename,"wb")
-        pkl.dump(self.get_numpy_dict(),filehandler)
+        pkl.dump(self.get_numpy_dictionary(),filehandler)
         filehandler.close()
-
-        
