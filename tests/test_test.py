3917cf692bc2e30350ba7661f89300af33987082
belsten <afbelsten@gmail.com>
2022-05-05 07:49:35 -0700

    initial upload of testing utilities base class and a test of that base class using np and torch arrays

---
+++ tests/test_test.py
@@ -0,0 +1,36 @@
+from tests.testing_utilities import TestCase
+import torch
+import numpy as np
+
+
+class TestTestCaseBaseClass(TestCase):
+
+    def test_pytorch_all_close(self):
+        result = torch.ones([100,100]) + 1e-10
+        expected = torch.ones([100,100])
+        self.assertAllClose(result, expected)
+        
+    def test_np_all_close(self):
+        result = np.ones([100,100]) + 1e-10
+        expected = np.ones([100,100])
+        self.assertAllClose(result, expected)
+
+    def test_assert_true(self):
+        self.assertTrue(True==True)
+        
+    def test_assert_false(self):
+        self.assertFalse(True!=True)
+        
+    def test_assert_equal(self):
+        self.assertEqual('sparse coding','sparse coding')
+
+    def test_assert_pytorch_shape_equal(self):
+        a = torch.zeros([100,100]) 
+        b = torch.ones([100,100])
+        self.assertShapeEqual(a,b)
+        
+    def test_assert_np_shape_equal(self):
+        a = np.zeros([100,100]) 
+        b = np.ones([100,100])
+        self.assertShapeEqual(a,b)
+        
---
+++ tests/testing_utilities.py
@@ -0,0 +1,18 @@
+import numpy as np
+import unittest
+
+
+## constants
+default_atol = 1e-6
+default_rtol = 1e-5
+
+
+class TestCase(unittest.TestCase):
+    '''Base class for testing'''
+
+    def assertAllClose(self, a, b, rtol=default_rtol, atol=default_atol):
+        return np.testing.assert_allclose(a, b, rtol=rtol, atol=atol)
+
+    def assertShapeEqual(self, a, b):
+        assert a.shape == b.shape
+        
