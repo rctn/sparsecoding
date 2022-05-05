4df42d1ee68bb83e2a3f656e2a2734b89b50826f
belsten <afbelsten@gmail.com>
2022-05-05 12:49:00 -0700

    evaluate LCA, Vanilla with cuda and cp, verify output dims, and approximate inference quality

--- tests/testing_utilities.py
+++ tests/testing_utilities.py
@@ -18,20 +18,3 @@ class TestCase(unittest.TestCase):
     def assertShapeEqual(self, a, b):
         assert a.shape == b.shape
         
-        
-        
-class CommonInferenceTests(object):
-    '''Base class for evaluating inference method'''
-    
-    def assertBarInference(self,inference_method,patch_size=8,n_samples=2,coefficient_treshold=0.8,device=torch.device('cpu')):
-        cpudevice = torch.device('cpu')
-
-        bars = BarsDataset(patch_size=patch_size,
-                           n_samples=n_samples,
-                           device=device,
-                           coefficient_treshold=coefficient_treshold
-        )
-        a = inference_method.infer(bars.data,bars.dictionary)
-
-        self.assertAllClose(a.to(cpudevice),bars.coefficients.to(cpudevice))
-    
