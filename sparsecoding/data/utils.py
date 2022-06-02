4b35985e94bdbeb3ed8b05a92c13f1304603363b
Alexander Belsten <afbelsten@gmail.com>
2022-06-02 10:26:53 -0700

    renamed van hateren to olshausen

--- sparsecoding/data/utils.py
+++ sparsecoding/data/utils.py
@@ -8,7 +8,7 @@ DATASET_PATH = os.path.join(MODULE_PATH, "datasets")
 DICTIONARY_PATH = os.path.join(MODULE_PATH, "dictionaries")
 
 BARS_DICT_PATH = os.path.join(DICTIONARY_PATH, "bars", "bars-16_by_16.p")
-VH_DICT_PATH = os.path.join(DICTIONARY_PATH, "van_hateren", "VH-1.5x_overcomplete.p")
+OLSHAUSEN_DICT_PATH = os.path.join(DICTIONARY_PATH, "olshausen", "olshausen-1.5x_overcomplete.p")
 
 
 def load_dictionary_from_pickle(path):
@@ -28,8 +28,8 @@ def load_bars_dictionary():
     return dictionary
 
 
-def load_van_hateren_dictionary():
-    path = VH_DICT_PATH
+def load_olshausen_dictionary():
+    path = OLSHAUSEN_DICT_PATH
     dictionary_file = open(path, 'rb')
     numpy_dictionary = pkl.load(dictionary_file)
     dictionary_file.close()
