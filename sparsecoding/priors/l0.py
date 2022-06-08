a690dde0599283044aa8437402af2b552217a29b
alvinzz <alvn.zng@gmail.com>
2022-06-07 21:55:03 -0700

    add L0IidPrior

--- sparsecoding/priors/l0.py
+++ sparsecoding/priors/l0.py
@@ -77,4 +77,94 @@ class L0Prior(Prior):
 
 # TODO: Add L0ExpPrior, where the number of active units is distributed exponentially.
 
-# TODO: Add L0IidPrior, where the magnitude of an active unit is distributed according to an i.i.d. Prior.
+class L0IidPrior(Prior):
+    """L0-sparse Prior with non-binary weights.
+    
+    If a weight is active, its value is drawn from an i.i.d. Prior.
+
+    Parameters
+    ----------
+    prob_distr : Tensor, shape [D], dtype float32
+        Probability distribution over the l0-norm of the weights.
+    active_weight_prior : Prior
+        The distribution for active weights.
+        Since weights drawn from this distribution must be i.i.d.,
+            we enforce `active_weight_prior.D` to be 1.
+    """
+
+    def __init__(
+        self,
+        prob_distr: torch.Tensor,
+        active_weight_prior: Prior,
+    ):
+        if prob_distr.dim() != 1:
+            raise ValueError(f"`prob_distr` shape must be (D,), got {prob_distr.shape}.")
+        if prob_distr.dtype != torch.float32:
+            raise ValueError(f"`prob_distr` dtype must be torch.float32, got {prob_distr.dtype}.")
+        if not torch.allclose(torch.sum(prob_distr), torch.ones(1, dtype=torch.float32)):
+            raise ValueError(f"`torch.sum(prob_distr)` must be 1., got {torch.sum(prob_distr)}.")
+        if active_weight_prior.D != 1:
+            raise ValueError(
+                f"`active_weight_prior.D` must be 1 (got {active_weight_prior.D}). "
+                f"This enforces that can sample i.i.d. weights."
+            )
+
+        self.prob_distr = prob_distr
+        self.active_weight_prior = active_weight_prior
+
+    @property
+    def D(self):
+        return self.prob_distr.shape[0]
+
+    def sample(
+        self,
+        num_samples: int,
+    ):
+        N = num_samples
+
+        num_active_weights = 1 + torch.multinomial(
+            input=self.prob_distr,
+            num_samples=num_samples,
+            replacement=True,
+        )  # [N]
+
+        d_idxs = torch.arange(self.D)
+        active_idx_mask = (
+            d_idxs.reshape(1, self.D)
+            < num_active_weights.reshape(N, 1)
+        )  # [N, self.D]
+
+        n_idxs = torch.arange(N).reshape(N, 1).expand(N, self.D)  # [N, D]
+        # Need to shuffle here so that it's not always the first weights that are active.
+        shuffled_d_idxs = [torch.randperm(self.D) for _ in range(N)]
+        shuffled_d_idxs = torch.stack(shuffled_d_idxs, dim=0)  # [N, D]
+
+        # [num_active_weights], [num_active_weights]
+        active_weight_idxs = n_idxs[active_idx_mask], shuffled_d_idxs[active_idx_mask]
+
+        weights = torch.zeros((N, self.D), dtype=torch.float32)
+        n_active_idxs = int(torch.sum(active_idx_mask).cpu().numpy())
+        active_weight_values = self.active_weight_prior.sample(n_active_idxs)
+        weights[active_weight_idxs] += active_weight_values.reshape(-1)
+
+        return weights
+
+    def log_prob(
+        self,
+        sample: torch.Tensor,
+    ):
+        super().check_sample_input(sample)
+
+        active_weight_mask = (sample != 0.)
+
+        l0_norm = torch.sum(active_weight_mask, dim=1).type(torch.long)  # [num_samples]
+        log_prob = torch.log(self.prob_distr[l0_norm - 1])
+        log_prob[l0_norm == 0] = -torch.inf
+
+        active_log_prob = (
+            self.active_weight_prior.log_prob(sample.reshape(-1, 1)).reshape(sample.shape)
+        )  # [num_samples, D]
+        active_log_prob[~active_weight_mask] = 0.  # [num_samples, D]
+        log_prob += torch.sum(active_log_prob, dim=1)  # [num_samples]
+
+        return log_prob
--- tests/priors/test_l0.py
+++ tests/priors/test_l0.py
@@ -3,7 +3,8 @@ from itertools import product
 import torch
 import unittest
 
-from sparsecoding.priors.l0 import L0Prior
+from sparsecoding.priors.l0 import L0Prior, L0IidPrior
+from sparsecoding.priors.laplace import LaplacePrior
 
 
 class TestL0Prior(unittest.TestCase):
@@ -59,6 +60,75 @@ class TestL0Prior(unittest.TestCase):
         assert torch.allclose(
             log_probs[[3, 5, 6]],
             torch.log(torch.tensor(0.25)),
+        )
+        assert log_probs[7] == -torch.inf
+
+
+class TestL0IidPrior(unittest.TestCase):
+    def test_sample(self):
+        N = 10000
+        prob_distr = torch.tensor([0.5, 0.25, 0, 0.25])
+        iid_prior = LaplacePrior(dim=1, scale=1, positive_only=True)
+
+        torch.manual_seed(1997)
+
+        D = prob_distr.shape[0]
+
+        l0_iid_prior = L0IidPrior(prob_distr, iid_prior)
+        weights = l0_iid_prior.sample(N)
+
+        assert weights.shape == (N, D)
+
+        # Check uniform distribution over which weights are active.
+        per_weight_hist = torch.sum(weights, dim=0)  # [D]
+        normalized_per_weight_hist = per_weight_hist / torch.sum(per_weight_hist)  # [D]
+        assert torch.allclose(
+            normalized_per_weight_hist,
+            torch.full((D,), 0.25, dtype=torch.float32),
+            atol=1e-2,
+        )
+
+        # Check the distribution over the l0-norm of the weights.
+        num_active_per_sample = torch.sum(weights != 0., dim=1)  # [N]
+        for num_active in range(1, 5):
+            assert torch.allclose(
+                torch.sum(num_active_per_sample == num_active) / N,
+                prob_distr[num_active - 1],
+                atol=1e-2,
+            )
+        
+        # Check Laplacian distribution.
+        laplace_weights = weights[weights > 0.]
+        for quantile in torch.arange(5) / 5.:
+            cutoff = -torch.log(1. - quantile)
+            assert torch.allclose(
+                torch.sum(laplace_weights < cutoff) / (N * torch.sum(prob_distr * (1 + torch.arange(D)))),
+                quantile,
+                atol=1e-2,
+            )
+
+    def test_log_prob(self):
+        prob_distr = torch.tensor([0.75, 0.25, 0.])
+        iid_prior = LaplacePrior(dim=1, scale=1, positive_only=True)
+
+        l0_iid_prior = L0IidPrior(prob_distr, iid_prior)
+
+        samples = list(product([0, 1], repeat=3))  # [2**D, D]
+        samples = torch.tensor(samples, dtype=torch.float32)  # [2**D, D]
+
+        log_probs = l0_iid_prior.log_prob(samples)
+
+        # The l0-norm at index `i`
+        # is the number of ones
+        # in the binary representation of `i`.
+        assert log_probs[0] == -torch.inf
+        assert torch.allclose(
+            log_probs[[1, 2, 4]],
+            torch.log(torch.tensor(0.75)) - 1.,
+        )
+        assert torch.allclose(
+            log_probs[[3, 5, 6]],
+            torch.log(torch.tensor(0.25)) - 2.,
         )
         assert log_probs[7] == -torch.inf
 
