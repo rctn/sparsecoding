5c1a03c00ecf56937de699407921e900db0f8bf1
HarrisonSantiago <harrisonsantiago4@gmail.com>
2024-11-16 11:01:55 -0500

    fix

--- sparsecoding/transforms/__init__.py
+++ sparsecoding/transforms/__init__.py
@@ -1,6 +1,6 @@
 from .patch import quilt, patchify
-from .whiten import whiten, WhiteningTransform, compute_whitening_stats
-from .images import whiten_images
+from .whiten import whiten, compute_whitening_stats
+from .images import whiten_images, WhiteningTransform
 
 __all__ = ['quilt', 'patchify', 'whiten',
            'compute_whitening_stats', 'WhiteningTransform',
