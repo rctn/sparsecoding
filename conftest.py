b441ae44412ad415c456b3ff4c00f985fbbc7632
Dylan <dpaiton@gmail.com>
2024-12-31 13:17:21 -0800

    formatting

--- conftest.py
+++ conftest.py
@@ -1,15 +1,20 @@
 import pytest
 import torch
 
-from sparsecoding.test_utils import (bars_datas_fixture, bars_datasets_fixture,
-                                     bars_dictionary_fixture,
-                                     dataset_size_fixture, patch_size_fixture,
-                                     priors_fixture)
+from sparsecoding.test_utils import (
+    bars_datas_fixture,
+    bars_datasets_fixture,
+    bars_dictionary_fixture,
+    dataset_size_fixture,
+    patch_size_fixture,
+    priors_fixture,
+)
 
 
 @pytest.fixture(autouse=True)
 def set_seed():
     torch.manual_seed(1997)
+
 
 # We import and define all fixtures in this file.
 # This allows users to avoid any dependency fixtures.
