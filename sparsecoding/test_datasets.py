29895033491fcedcc9c0bf9667d964fd43c97d88
Alexander Belsten <belsten@berkeley.edu>
2025-02-28 15:30:38 -0800

    flake

--- sparsecoding/test_datasets.py
+++ sparsecoding/test_datasets.py
@@ -1,12 +1,12 @@
 import pytest
-
 from sparsecoding.datasets import FieldDataset
+
 
 @pytest.mark.parametrize("whitened", [True, False])
 def test_FieldDataset(
         patch_size_fixture: int,
         dataset_size_fixture: int,
-        whitened:bool,
+        whitened: bool,
 ):
     fielddataset = FieldDataset(
         root="data",
